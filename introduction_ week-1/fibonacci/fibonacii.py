360c1f96c0c1c0896da35c929866c8a1a31e9f2b
Prakhar Dixit <prakhhar1997@gmail.com>
2018-01-03 08:56:22 +0530

    testcase-1

---
+++ introduction_ week-1/fibonacci/fibonacii.py
@@ -0,0 +1,21 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Prakhar Dixit\Downloads\_e65d1ca299e1f786541ead24455543f2_01_introduction_starter_files\fibonacci\fibonacci.py 
+7
+13
+>>> 
+ RESTART: C:\Users\Prakhar Dixit\Downloads\_e65d1ca299e1f786541ead24455543f2_01_introduction_starter_files\fibonacci\fibonacci.py 
+7
+Traceback (most recent call last):
+  File "C:\Users\Prakhar Dixit\Downloads\_e65d1ca299e1f786541ead24455543f2_01_introduction_starter_files\fibonacci\fibonacci.py", line 12, in <module>
+    print(calc_fib(n))
+  File "C:\Users\Prakhar Dixit\Downloads\_e65d1ca299e1f786541ead24455543f2_01_introduction_starter_files\fibonacci\fibonacci.py", line 7, in calc_fib
+    for i in xrange(2,n+1):
+NameError: name 'xrange' is not defined
+>>> 
+>>> 
+ RESTART: C:\Users\Prakhar Dixit\Downloads\_e65d1ca299e1f786541ead24455543f2_01_introduction_starter_files\fibonacci\fibonacci.py 
+7
+13
+>>> 
